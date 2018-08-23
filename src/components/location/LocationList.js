6226accb4e46516c3a210debe6e2e11a2e5df8fd
Daniel Brewer <danielbrewer2014@comcast.net>
2018-08-23 12:56:46 -0500

    first react ex

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList  extends Component {
+    render() {
+        return (
+            <article>
+                <h2>Location List</h2>
+                <section><h3>Nashville North</h3>
+                <h4>123 main st</h4>
+                </section>
+                <section><h3>Nashville South</h3>
+                <h4>345 main st</h4>
+                </section>
+                
+            
+
+            </article>
+        );
+    }
+}
